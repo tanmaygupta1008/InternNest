9588526f41eb6971519c56a22bdd4d7cb18e01a0
Arshvir Singh Kalsi <arshvirsk26@gmail.com>
2025-02-24 01:15:51 +0530

    Changing Passowrd :)

--- internshala_clone/settings.py
+++ internshala_clone/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "internshala",
         "USER": "root",
-        "PASSWORD": "Arshviro$709",
+        "PASSWORD": "1234",
         "HOST": "localhost",  # or the hostname where your MySQL server is running
         "PORT": "3306",  # or the port on which your MySQL server is listening
     }
